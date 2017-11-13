1c6998fc855ed31d313e90950d212e97e3c7bfa5
Dj T@l <guillaume.garcera@gmail.com>
2017-11-13 17:25:40 +0100

    Update install.sh
    * update bash to homebrew one
    * copy some specific stuff

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ for name in *; do
       fi
     fi
   else
-    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ] && [ "$name" != "misc" ]; then
+    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ] && [ "$name" != "misc" ] && [ "$name" != "init" ]; then
       echo "Creating $target"
       if [ -n "$(grep "$cutstring" "$name")" ]; then
         cp "$PWD/$name" "$target"
@@ -35,3 +35,13 @@ for name in *; do
     fi
   fi
 done
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
+
+# Copy some preferences too that shoud be stored in init folder
+# All file in init are specific so each deserve it's way to be installed
+cp -r init/spectacle.json ~/Library/Application\ Support/Spectacle/Shortcuts.json 2> /dev/null
