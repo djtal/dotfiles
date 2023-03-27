5794d5851a0ea3341f69da51cfede58b4298c1ec
Guillaume Garcera <guillaume.garcera@gmail.com>
2023-03-27 17:21:50 +0200

    Update keybings for gitsigns

--- config/nvim/lua/git_signs.lua
+++ config/nvim/lua/git_signs.lua
@@ -39,4 +39,15 @@ require('gitsigns').setup {
   yadm = {
     enable = false
   },
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    map('n', '<leader>tb', gs.toggle_current_line_blame)
+  end
 }
