68b50507f5e2ca2353a3bc384ac1464d109f7782
Guillaume Garcera <guillaume.garcera@gmail.com>
2023-05-04 09:10:25 +0200

    Add null_ls config

--- config/nvim/lua/nulls_config.lua
+++ config/nvim/lua/nulls_config.lua
@@ -2,6 +2,7 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
   on_attach = function(client, bufnr)
+    -- Allow formating if supported by client
     if client.supports_method("textDocument/formatting") then
       vim.keymap.set("n", "<Leader>f", function()
         vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
@@ -10,7 +11,9 @@ null_ls.setup({
   end,
 
   sources = {
+    null_ls.builtins.diagnostics.yamllint,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.prettier,
+    null_ls.builtins.diagnostics.erb_lint,
   },
 })
